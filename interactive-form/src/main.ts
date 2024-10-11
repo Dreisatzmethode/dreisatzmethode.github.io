b0aaec6936a0ed3dbc32a109a22998e078f1a6d8
Gian Ulli <info@gianulli.ch>
2024-10-11 15:26:06 +0200

    Start with Vueform

---
+++ interactive-form/src/main.ts
@@ -0,0 +1,26 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+// FormKit imports
+import { plugin as formKitPlugin, defaultConfig } from "@formkit/vue";
+import { createMultiStepPlugin } from "@formkit/addons";
+import "@formkit/themes/genesis";
+import "@formkit/addons/css/multistep";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(
+  formKitPlugin,
+  defaultConfig({
+    plugins: [createMultiStepPlugin()],
+  })
+);
+
+app.mount("#app");
